b40c1680a53518245728b1ecf2a8cd7e14af55c4
Muhammed Roshan <roshantac@gmail.com>
2021-05-11 10:54:46 +0530

    Create Push_Zeros_to_end.c

---
+++ Push_Zeros_to_end.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+int a[20]={0,2,0,7,3,0,4,0,0,9,4,0},n=12,b[20],k=0;
+    for (int i=0;i<n;i++)
+    if(a[i]!=0 )
+    {
+        
+        b[k]=a[i];k++;
+    }
+    for(int i=k;i<n;i++)
+    b[i]=0;
+    for(int i=0;i<n;i++)
+    printf("%d ,",b[i]);
+    return 0;
+}
