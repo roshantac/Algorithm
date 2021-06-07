e4ec1f16ca2d5fc8b4c08f2bc83f56f4a4d13123
Muhammed Roshan <roshantac@gmail.com>
2021-06-07 10:53:56 +0530

    Update reverse_of_sentence.c

--- reverse_of_sentence.c
+++ reverse_of_sentence.c
@@ -7,6 +7,31 @@ ouput : programing to welcome hello
 #include <stdio.h>
 #include <string.h>
 
+#define SOLUTION 1
+
+#ifdef SOLUTION
+int main()
+{
+    char string1[50];
+    int i=0;
+    printf("\n Enter the string: ");
+    gets(string1);
+    for(i= strlen(string1)-1;i >=0 ;i--)
+    {
+        if(string1[i]==' '|| i==0)
+        {
+            if(i==0) i--;
+            for (int j=i+1;string1[j]!=' ' && string1[j]!='\0';j++)
+                printf("%c",string1[j]);
+            printf(" ");
+        }
+    }
+    return 0;
+}
+
+#else
+
+
 char string_stack[20][20];
 int count = 0;
 void push(char *str)
@@ -46,5 +71,5 @@ for(i=count+1;i>=0;i--)
 }
     return 0;
 }
-
+#endif
 
